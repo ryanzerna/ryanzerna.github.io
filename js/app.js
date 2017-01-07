134d1f27c70d21bff39fe01ce389983f7ffa1780
Ryan Zerna <ryanzerna@Ryans-MacBook-Air.local>
2017-01-06 23:25:03 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+jQuery(function($){
+  $(document).ready(function(){
+
+    $(document).foundation();
+
+    if(!Cookies.get('video')) {
+      Cookies.set('video', 'true', { expires: 365, path: '' });
+      $('#videoModal').foundation('open');
+      setTimeout(function() {
+        $('#videoModal').foundation('close');
+      }, 27500);
+    }
+  });
+});
