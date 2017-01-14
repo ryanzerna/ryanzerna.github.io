e3f4d773a934eb2601ee1f7f5c31b5b3b0b51b86
Ryan Zerna <ryanzerna@gmail.com>
2017-01-14 10:22:20 -0600

    new updates

--- js/app.js
+++ js/app.js
@@ -4,12 +4,10 @@ jQuery(function($){
 
     $(document).foundation();
 
-    if(!Cookies.get('video')) {
-      Cookies.set('video', 'true', { expires: 1, path: '' });
-      $('#videoModal').foundation('open');
-      setTimeout(function() {
-        $('#videoModal').foundation('close');
-      }, 27500);
-    }
+    $('#videoModal').foundation('open');
+    setTimeout(function() {
+      $('#videoModal').foundation('close');
+    }, 27500);
+    
   });
 });
